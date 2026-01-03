1442bb5a6b282c004132b609863def3dd85e5a6d
Uzma383 <uzmashaikh1886@gmail.com>
2026-01-02 23:40:53 -0700

    Changes made in second

--- second.py
+++ second.py
@@ -12,3 +12,25 @@ print(fruits)
 
 fruits.sort()
 print(fruits)
+
+# Similary, sort works on the list with only numbers as well
+nums = [12, 4, 2, 11, 1, 100]
+print(nums)
+nums.sort()
+print(nums)
+
+#checking length
+countries = ['Russia', 'India', 'US', 'Japan', 'Germany', 'Italy', 'Zambia']
+print(len(countries))
+# Returns the one with highest alphabet, when list is made of strings
+print(max(countries))   
+print(min(countries))
+
+# Let's define a simple for loop
+
+for item in countries:            
+  print(item)
+
+
+for item in countries:
+      print(f'{item} --- {item.upper()} --- {len(item)}')   
