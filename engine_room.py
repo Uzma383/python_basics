e6331b5bb8f0f8542f8c76fe3fe1a33d12dd766e
Uzma383 <uzmashaikh1886@gmail.com>
2026-01-02 23:14:50 -0700

    first python code

---
+++ engine_room.py
@@ -0,0 +1,26 @@
+num1 = 3
+num2 = 3.0 
+print(type(num1))
+print(type(num2))
+
+
+text = "Welcome"
+print(text[2:5])
+print(text[:2])
+print(text[2:])
+
+
+a = '2'
+b = '3'
+print(a, type(a))
+print(b, type(b))
+print('Addition = ', a+b)   
+
+print()                     
+
+a = int(a)
+b = int(b)
+print(a, type(a))       
+print(b, type(b))       
+print('Addition = ', a+b)
+
