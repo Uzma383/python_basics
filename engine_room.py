8e8f9e56289f026fde85e892a0535ce59decb80e
Uzma383 <uzmashaikh1886@gmail.com>
2026-01-02 23:33:08 -0700

    Changes made

--- engine_room.py
+++ engine_room.py
@@ -24,3 +24,35 @@ print(a, type(a))
 print(b, type(b))       
 print('Addition = ', a+b)
 
+#Try passing some integer as input and check its datatype.
+num = input('enter some integer  -  ')   
+print(num)
+print(type(num))
+
+# Carry addition of two user-inputted numbers
+num1 = input('Enter first number:  ')
+num2 = input('Enter second number:  ')
+print(num1 + num2)    
+print(int(num1) + int(num2)) 
+
+
+# multiple if else => use elif
+ 
+height = 176
+if height < 140:
+    print('too short')
+elif height < 180:
+    print('height is ok')
+else:
+    print('too tall')
+
+
+
+# You can pass boolean value (True or False) to the condition in if. In that case, you don't need any logical operator like == or !=
+
+
+is_subscribed = False
+if is_subscribed:             
+    print('You  subscribed!')  
+else:
+    print('Please subscribe')
---
+++ second.py
@@ -0,0 +1,14 @@
+fruits = ['apple','banana','kiwi']
+print(fruits)
+
+print(type(fruits))
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[0:2])
+
+fruits.append('Mango')
+print(fruits)
+
+fruits.sort()
+print(fruits)
